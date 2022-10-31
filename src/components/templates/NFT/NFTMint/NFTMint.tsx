4543f682690fd0de54a74561fef4efd77d583f59
David Boller <david@virtual-graphics.ch>
2022-10-31 11:13:10 +0100

    Added NFT Mint section

--- src/components/templates/NFT/NFTMint/NFTMint.tsx
+++ src/components/templates/NFT/NFTMint/NFTMint.tsx
@@ -1,7 +1,20 @@
-import { Container, Stack, Box, Text, Flex, Heading, useColorModeValue as mode } from '@chakra-ui/react';
+import { Box,
+  Button,
+  Flex,
+  Container,
+  Divider,
+  Heading,
+  Input,
+  Stack,
+  Text,
+  Image,
+
+  useBreakpointValue, useColorModeValue as mode } from '@chakra-ui/react';
+
 import { FC, useEffect } from 'react';
 
 const NFTMint= () => {
+  
    return (
     <Box w="100%" > <Flex align="center" justify="center">
       <Heading
@@ -14,8 +27,46 @@ const NFTMint= () => {
                   >
                    Mint your NFT
                   </Heading>
-                  
-        </Flex>
+                  </Flex>
+        <Box py={{ base: '12', md: '24' }}>
+    <Container
+      maxW="md"
+      py={{ base: '0', sm: '8' }}
+      px={{ base: '4', sm: '10' }}
+      bg={useBreakpointValue({ base: 'transparent', sm: 'bg-surface' })}
+      boxShadow={{ base: 'none', sm: 'xl' }}
+      borderRadius={{ base: 'none', sm: '2xl' }}
+    >
+      <Stack spacing="8">
+        <Stack spacing="6" align="center">
+         
+          <Stack spacing="3" textAlign="center">
+            <Image borderRadius="20" src="https://www.virtual-graphics.ch/wp-content/uploads/2022/10/collection.jpg" alt="Varan"/>
+           
+          </Stack>
+        </Stack>
+        <Stack spacing="6" align="center">
+          <Text fontSize="md" color="muted" >
+            Available Mints
+          </Text>
+          
+          <Stack spacing="4">
+            
+            <Button colorScheme='teal' variant='solid' size="lg">Connect your Wallet</Button>
+          </Stack>
+        </Stack>
+        <Stack spacing="0.5" align="center">
+          <Text fontSize="sm" color="muted">
+           You can mint up to 5 NFTs
+          </Text>
+          
+        </Stack>
+        
+      </Stack>
+    </Container>
+  </Box>
+        
+        
       </Box>
   );
 };
